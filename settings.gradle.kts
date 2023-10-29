93e2372ac3015a3682f836e5530dc0eae9d43465
shreyas.sn <shreyas.sn@zohocorp.com>
2023-10-29 12:35:07 +0530

    Resolve gradle plugin issue

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,24 +1,17 @@
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
+        google()
         mavenCentral()
         gradlePluginPortal()
     }
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven { url =  uri("https://maven.google.com") } // For Gradle < 4.0
     }
 }
 
-rootProject.name = "MonthCalendarWidgetDemo"
 include(":app")
 include(":monthcalendar")
